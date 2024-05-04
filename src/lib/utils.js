51d25abea9c0113d64302a24da1fa928e8a74e8c
AdrienVerwaerde <adrien.verwaerde@oclock.school>
2024-05-04 13:51:54 +0200

    back to classic link

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -9,7 +9,7 @@ export const connectToDb = async () => {
             console.log("Using existing connection");
             return;
         }
-        const db = await mongoose.connect(process.env.MONGO);
+        const db = await mongoose.connect("mongodb+srv://verwaerdeadrien:Sanada1122@cluster0.btbshng.mongodb.net/next-app?retryWrites=true&w=majority");
         connection.isConnected = db.connections[0].readyState;
     } catch (error) {
         console.log(error);
