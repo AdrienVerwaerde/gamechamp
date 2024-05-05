a611df09ebab0b4574e1e044ff48b09d3ee81bf8
AdrienVerwaerde <adrien.verwaerde@oclock.school>
2024-05-05 19:05:28 +0200

    test 1000

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -9,7 +9,7 @@ export const connectToDb = async () => {
             console.log("Using existing connection");
             return;
         }
-        const db = await mongoose.connect(process.env.MONGO);
+        const db = await mongoose.connect(process.env.MONGO_CONNECT_URI);
         connection.isConnected = db.connections[0].readyState;
     } catch (error) {
         console.log(error);
